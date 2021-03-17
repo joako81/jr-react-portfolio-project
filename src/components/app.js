50f3349793ea7487096f42a838a7efbf1c4a86ce
joako81 <joaquin.rosa.82@gmail.com>
2021-03-17 17:55:24 +0100

    update pending bugs

--- src/components/app.js
+++ src/components/app.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import { library } from "@fortawesome/fontawesome-svg-core";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { 
-faTrash,
-faSignOutAlt,
-faEdit } from "@fortawesome/free-solid-svg-icons";
+
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
+import Icons from "../helpers/icons";
 
-library.add(faTrash, faSignOutAlt, faEdit);
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    Icons();
 
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN"
@@ -118,11 +118,23 @@ export default class App extends Component {
                   />
                 )}
               />
-              <Route path="/blog" component={Blog} />   
+              <Route path="/blog" 
+              render= {props => (
+                <Blog {...props} loggedInStatus = {this.state.loggedInStatus}/>
+
+              )}
+              
+              />
+
+
+
+              <Route path="/b/:slug" component={BlogDetail} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? (
+              this.autorizedPages()) : null}
+   
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               
-              {this.state.loggedInStatus === "LOGGED_IN" ? (this.autorizedPages()) : null}
 
 
               
@@ -130,7 +142,7 @@ export default class App extends Component {
 
               <Route
                 exact
-                path=":slug"
+                path="portfolio:slug"
                 component={PortfolioDetail}
               />
               <Route component={NoMatch} />
--- src/components/navigation/prueba.js
+++ src/components/navigation/prueba.js
@@ -1,220 +1,134 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import DropzoneComponent from "react-dropzone-component";
+import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
-import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
-import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
-
-export default class PortfolioForm extends Component {
-  constructor(props) {
-    super(props);
+class Blog extends Component {
+  constructor() {
+    super();
 
     this.state = {
-      name: "",
-      description: "",
-      category: "eCommerce",
-      position: "",
-      url: "",
-      thumb_image: "",
-      banner_image: "",
-      logo: ""
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: true,
+      blogModalIsOpen: false
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.componentConfig = this.componentConfig.bind(this);
-    this.djsConfig = this.djsConfig.bind(this);
-    this.handleThumbDrop = this.handleThumbDrop.bind(this);
-    this.handleBannerDrop = this.handleBannerDrop.bind(this);
-    this.handleLogoDrop = this.handleLogoDrop.bind(this);
-
-    this.thumbRef = React.createRef();
-    this.bannerRef = React.createRef();
-    this.logoRef = React.createRef();
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+    this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(
+      this
+    );
   }
 
-  handleThumbDrop() {
-    return {
-      addedfile: file => this.setState({ thumb_image: file })
-    };
-  }
-
-  handleBannerDrop() {
-    return {
-      addedfile: file => this.setState({ banner_image: file })
-    };
-  }
-
-  handleLogoDrop() {
-    return {
-      addedfile: file => this.setState({ logo: file })
-    };
-  }
-
-  componentConfig() {
-    return {
-      iconFiletypes: [".jpg", ".png"],
-      showFiletypeIcon: true,
-      postUrl: "https://httpbin.org/post"
-    };
-  }
-
-  djsConfig() {
-    return {
-      addRemoveLinks: true,
-      maxFiles: 1
-    };
-  }
-
-  buildForm() {
-    let formData = new FormData();
-
-    formData.append("portfolio_item[name]", this.state.name);
-    formData.append("portfolio_item[description]", this.state.description);
-    formData.append("portfolio_item[url]", this.state.url);
-    formData.append("portfolio_item[category]", this.state.category);
-    formData.append("portfolio_item[position]", this.state.position);
-
-    if (this.state.thumb_image) {
-      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
-    }
-
-    if (this.state.banner_image) {
-      formData.append("portfolio_item[banner_image]", this.state.banner_image);
-    }
-
-    if (this.state.logo) {
-      formData.append("portfolio_item[logo]", this.state.logo);
-    }
-
-    return formData;
-  }
-
-  handleChange(event) {
+  handleSuccessfulNewBlogSubmission(blog) {
     this.setState({
-      [event.target.name]: event.target.value
+      blogModalIsOpen: false,
+      blogItems: [blog].concat(this.state.blogItems)
     });
   }
 
-  handleSubmit(event) {
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    });
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    });
+  }
+
+  onScroll() {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
+    }
+  }
+
+  getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+
     axios
-      .post(
-        "https://jordan.devcamp.space/portfolio/portfolio_items",
-        this.buildForm(),
-        { withCredentials: true }
+      .get(
+        `https://joaquinrosa.devcamp.space/portfolio/portfolio_blogs?page=${this
+          .state.currentPage}`,
+        {
+          withCredentials: true
+        }
       )
       .then(response => {
-        this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
-
+        console.log("gettting", response.data);
         this.setState({
-          name: "",
-          description: "",
-          category: "eCommerce",
-          position: "",
-          url: "",
-          thumb_image: "",
-          banner_image: "",
-          logo: ""
-        });
-
-        [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
-          ref.current.dropzone.removeAllFiles();
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+          totalCount: response.data.meta.total_records,
+          isLoading: false
         });
       })
       .catch(error => {
-        console.log("portfolio form handleSubmit error", error);
+        console.log("getBlogItems error", error);
       });
+  }
 
-    event.preventDefault();
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
   }
 
   render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+
     return (
-      <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
-        <div className="two-column">
-          <input
-            type="text"
-            name="name"
-            placeholder="Portfolio Item Name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
+      <div className="blog-container">
+        <BlogModal
+          handleSuccessfulNewBlogSubmission={
+            this.handleSuccessfulNewBlogSubmission
+          }
+          handleModalClose={this.handleModalClose}
+          modalIsOpen={this.state.blogModalIsOpen}
+        />
 
-          <input
-            type="text"
-            name="url"
-            placeholder="URL"
-            value={this.state.url}
-            onChange={this.handleChange}
-          />
-        </div>
+        {this.props.loggedInStatus === "LOGGED_IN" ? (
+          <div className="new-blog-link">
+            <a onClick={this.handleNewBlogClick}>
+              <FontAwesomeIcon icon="plus-circle" />
+            </a>
+          </div>
+        ) : null}
 
-        <div className="two-column">
-          <input
-            type="text"
-            name="position"
-            placeholder="Position"
-            value={this.state.position}
-            onChange={this.handleChange}
-          />
+        <div className="content-container">{blogRecords}</div>
 
-          <select
-            name="category"
-            value={this.state.category}
-            onChange={this.handleChange}
-            className="select-element"
-          >
-            <option value="eCommerce">eCommerce</option>
-            <option value="Scheduling">Scheduling</option>
-            <option value="Enterprise">Enterprise</option>
-          </select>
-        </div>
-
-        <div className="one-column">
-          <textarea
-            type="text"
-            name="description"
-            placeholder="Description"
-            value={this.state.description}
-            onChange={this.handleChange}
-          />
-        </div>
-
-        <div className="image-uploaders">
-          <DropzoneComponent
-            ref={this.thumbRef}
-            config={this.componentConfig()}
-            djsConfig={this.djsConfig()}
-            eventHandlers={this.handleThumbDrop()}
-          >
-            <div className="dz-message">Thumbnail</div>
-          </DropzoneComponent>
-
-          <DropzoneComponent
-            ref={this.bannerRef}
-            config={this.componentConfig()}
-            djsConfig={this.djsConfig()}
-            eventHandlers={this.handleBannerDrop()}
-          >
-            <div className="dz-message">Banner</div>
-          </DropzoneComponent>
-
-          <DropzoneComponent
-            ref={this.logoRef}
-            config={this.componentConfig()}
-            djsConfig={this.djsConfig()}
-            eventHandlers={this.handleLogoDrop()}
-          >
-            <div className="dz-message">Logo</div>
-          </DropzoneComponent>
-        </div>
-
-        <div>
-          <button className="btn" type="submit">
-            Save
-          </button>
-        </div>
-      </form>
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
+      </div>
     );
   }
 }
+
+export default Blog;
